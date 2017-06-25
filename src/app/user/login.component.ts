b8f3dc28aeb2c8beb03b08a48352e3a887ed7f92
bibotai <bibotaixsf@gmail.com>
2017-06-25 15:39:56 +0800

    Added login page styls

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -2,42 +2,7 @@ import {Component, ElementRef} from '@angular/core';
 
 import {AuthorizationService} from '../services/authorization.service'
 
-@Component({selector: 'login-form', template: `
-        <div class="container" >
-            <div class="title">
-                Welcome
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input  id="username" 
-                            type="test" [(ngModel)]="username" class="validate">
-                        <label for="username">username</label>
-                    </div>
-                </div>
- 
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input  id="password" [(ngModel)]="password"
-                            type="password" class="validate">
-                        <label for="password">Password</label>
-                    </div>
-                </div>
- 
-                <span>{{errorMsg}}</span>
-                <button (click)="login()" 
-                    class="btn waves-effect waves-light" 
-                    type="submit" name="action">Login</button>
-            </div>
-        </div>
-
-        <button (click)="encrypt()" 
-                    class="btn waves-effect waves-light" 
-                    type="button" name="action">encrypt</button>
-                     <button (click)="refresh()" 
-                    class="btn waves-effect waves-light" 
-                    type="button" name="action">refresh</button>
-    	`})
+@Component({selector: 'login-form', templateUrl: './login.component.html', styleUrls: ['./login.component.css']})
 
 export class LoginComponent {
     username : string;
